17cd4448469ee38fee757dfc2af1acca20dde3b8
Alexis <alxssnts@gmail.com>
2026-06-01 19:16:40 +0800

    Fixes / layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,9 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <AuthProvider>
-        <body className="min-h-full flex flex-col">{children}</body>
-      </AuthProvider>
+      <body className="min-h-full flex flex-col">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
