3504c71dc2f192b370fff2b18b2dc1c544ce47e8
Alexis <alxssnts@gmail.com>
2026-06-03 16:42:31 +0800

    Updated user form

--- app/admin/components/UserFormModal.tsx
+++ app/admin/components/UserFormModal.tsx
@@ -140,8 +140,9 @@ export default function UserFormModal({
               onChange={handleChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              <option value="USER">User</option>
               <option value="ADMIN">Admin</option>
+              <option value="ENCODER">Encoder</option>
+              <option value="USER">User</option>
             </select>
           </div>
 
